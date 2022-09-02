11fe9ac9480a2f0deb04c38cfa0f08fe6de24329
David Ram Menez <dcm.facul@gmail.com>
2022-09-01 21:42:22 -0300

    Update Lessons

--- laboratorioAEDSII/Theoretical LabII/0 - Introduction C/introductionC.c
+++ laboratorioAEDSII/Theoretical LabII/0 - Introduction C/introductionC.c
@@ -1,6 +1,7 @@
 //http://linguagemc.com.br/a-estrutura-de-repeticao-for-em-c/#:~:text=O%20la%C3%A7o%20for%20%C3%A9%20uma,loop%2C%20bem%20como%20seu%20incremento.
 //http://linguagemc.com.br/funcoes-em-c/
 //http://linguagemc.com.br/a-biblioteca-math-h/
+//https://www.inf.pucrs.br/~pinho/LaproI/Vetores/Vetores.htm
 
 //Bibliotecas da Linguagem
 //Estrutura Base
---
+++ laboratorioAEDSII/exercicesLabII/3 - NaturalNumberProduct/NaturalNumberProduct.c
@@ -0,0 +1,11 @@
+#include <stdio.h>      
+#include <stdlib.h>  
+void main()  
+{     
+    // use rand() function to generate the number  
+    printf (" The random number is: %d", rand());  
+    printf ("\n The random number is: %d", rand());  
+      
+    printf (" \n The random number is: %d", rand());  
+    printf ("\n The random number is: %d", rand());  
+}  
