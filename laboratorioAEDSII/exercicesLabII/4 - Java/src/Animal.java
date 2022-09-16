6428a30d1d045921c4feb5738a33699f099d274e
David Ram Menez <dcm.facul@gmail.com>
2022-09-15 22:07:10 -0300

    update lessons

---
+++ laboratorioAEDSII/exercicesLabII/4 - Java/src/Animal.java
@@ -0,0 +1,20 @@
+import java.security.KeyStore.Entry.Attribute;
+
+//Faça uma classe Animal
+public class Animal {
+    // método abstrato "fala"
+    public void fala() {
+        
+    }
+
+    //Crie um método par alterar o nome do animal
+    public void mudaNome() {
+        setName(nome);
+        getName(nome);
+    } 
+
+    public static void main(String[] args) throws Exception {
+        //Atributo privado “nome”
+        private String nome;     
+    }
+}
