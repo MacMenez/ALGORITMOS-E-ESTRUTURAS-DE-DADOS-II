dc4388cacbafe014a8e2a83ec55cd96cf959b9b5
David Ram Menez <dcm.facul@gmail.com>
2022-10-06 23:31:44 -0300

    Update App.java

--- laboratorioAEDSII/exercicesLabII/6 - JavaOrdenation/src/App.java
+++ laboratorioAEDSII/exercicesLabII/6 - JavaOrdenation/src/App.java
@@ -11,49 +11,65 @@ public class App {
         Long gerar_dados_tempoFinal = System.currentTimeMillis();
 
         System.out.println(Arrays.toString(vetorNumeroAleatorio));
-        System.out.printf("Vetor Aleatório - Tempo total em Segundos: %.5f segundos \n", (gerar_dados_tempoFinal - gerar_dados_tempoInicial)/1000.0);
+        System.out.printf("Vetor Aleatório - Tempo total em Segundos: %.5f segundos \n\n", (gerar_dados_tempoFinal - gerar_dados_tempoInicial)/1000.0);
 
+        vetorNumeroAleatorio = vetorRandom(tamanhoVetor);
         Long quickSort_tempoInicial = System.currentTimeMillis();
-            //Ordenação do vetor com QuickSort
-            ordenarQuickSort(vetorNumeroAleatorio, 0, vetorNumeroAleatorio.length-1);
+            //Parametro do vetorNumeroAleatorio para fazer QuickSort
+            int esq = 0, dir = vetorNumeroAleatorio.length-1;
+            
+            //Ordenação do vetor com QuickSort           
+            ordenarQuickSort(vetorNumeroAleatorio, esq, dir);
         Long quickSort_tempoFinal = System.currentTimeMillis();
 
-        System.out.println(Arrays.toString(vetorNumeroAleatorio)); //Como mostrar o vetor ordenado?
-        System.out.printf("QuickSort - Tempo total em Segundos: %.5f segundos \n", (quickSort_tempoFinal - quickSort_tempoInicial)/1000.0);
-            /*
+        System.out.println(Arrays.toString(vetorNumeroAleatorio));
+        System.out.printf("QuickSort - Tempo total em Segundos: %.5f segundos \n\n", (quickSort_tempoFinal - quickSort_tempoInicial)/1000.0);
 
-        Long heapSort_tempoInicial = System.currentTimeMillis();
+        vetorNumeroAleatorio = vetorRandom(tamanhoVetor);
+        Long heapSort_tempoInicial = System.currentTimeMillis();            
             //Ordenação do vetor com HeapSort
-            ordenarHeapSort();
+            ordenarHeapSort(vetorNumeroAleatorio, vetorNumeroAleatorio.length-1);
         Long heapSort_tempoFinal = System.currentTimeMillis();
 
-        System.out.println(Arrays.toString()); //Como mostrar o vetor ordenado?
-        System.out.printf("HeapSort - Tempo total em Segundos: %.5f segundos \n", (heapSort_tempoFinal - heapSort_tempoInicial)/1000.0);
+        System.out.println(Arrays.toString(vetorNumeroAleatorio)); 
+        System.out.printf("HeapSort - Tempo total em Segundos: %.5f segundos \n\n", (heapSort_tempoFinal - heapSort_tempoInicial)/1000.0);
 
         //Comparativo de Algoritmos de Ordenações
         vetorNumeroAleatorio = vetorRandom(tamanhoVetor);
         Long tempoComparacao_inicioQuickSort = System.currentTimeMillis();
             for (int i = 0; i < 10; i++) {
-                System.out.printf("Total de Execuções do QuickSort: %d \n", i);
-                vetor
-                ordenarQuickSort(vetorNumeroAleatorio);
+                System.out.printf("Total de Execuções do QuickSort: %d \n\n", i);
+                ordenarQuickSort(vetorNumeroAleatorio, esq, dir);
             }
         Long tempoComparacao_fimQuickSort = System.currentTimeMillis();
-        System.out.println(Arrays.toString());//Como mostrar o vetor ordenado?
+
+        System.out.println(Arrays.toString(vetorNumeroAleatorio));
         double tempoQuick;
-        System.out.printf("Tempo total em Segundos: %.5f segundos \n", tempoQuick = ((heapSort_tempoFinal - heapSort_tempoInicial)/1000.0));
+        System.out.printf("Tempo total em Segundos: %.5f segundos \n\n", tempoQuick = ((tempoComparacao_fimQuickSort - tempoComparacao_inicioQuickSort)/1000.0));
         
         vetorNumeroAleatorio = vetorRandom(tamanhoVetor);
         Long tempoComparacao_inicioHeapSort = System.currentTimeMillis();
             for (int i = 0; i < 10; i++) {
-                System.out.printf("Total de Execuções do HeapSort: %d \n", i);
-                ordenarHeapSort();
+                System.out.printf("Total de Execuções do HeapSort: %d \n\n", i);
+                ordenarHeapSort(vetorNumeroAleatorio, vetorNumeroAleatorio.length-1);
             }
         Long tempoComparacao_fimHeapSort = System.currentTimeMillis();
-        System.out.println(Arrays.toString());//Como mostrar o vetor ordenado?
+
+        System.out.println(Arrays.toString(vetorNumeroAleatorio));
         double tempoHeap;
-        System.out.printf("Tempo total em Segundos: %.5f segundos \n", tempoHeap = ((heapSort_tempoFinal - heapSort_tempoInicial)/1000.0));
-        */ }
+        System.out.printf("Tempo total em Segundos: %.5f segundos \n", tempoHeap = ((tempoComparacao_fimHeapSort - tempoComparacao_inicioHeapSort)/1000.0));
+
+        if (tempoQuick > tempoHeap) {
+            System.out.println("O método QuickSort realizou o procedimento em menor tempo, mostrando - se o melhor método.");
+            System.out.printf("\nTempo QuickSort: %d", tempoQuick);
+            System.out.printf("\nTempo HeapSort: %d", tempoHeap);
+        }
+        if (tempoQuick < tempoHeap) {
+            System.out.println("O método HeapSort realizou o procedimento em menor tempo, mostrando - se o melhor método.");
+            System.out.printf("\nTempo HeapSort: %d", tempoHeap);
+            System.out.printf("\nTempo QuickSort: %d", tempoQuick);
+        }
+    }
 
     public static double [] vetorRandom(int tamanhoVetor) {
         double [] vetorNumeroAleatorio;
@@ -68,50 +84,66 @@ public class App {
     }
 
     public static double [] ordenarQuickSort (double[] vetorNumeroAleatorio, int esquerda, int direita) {
-        int i = esquerda, j = direita;
+        int esq = esquerda, dir = direita;
+        double pivo = vetorNumeroAleatorio[(esq+dir)/2];
+        double swap;
 
-        double pivo = vetorNumeroAleatorio[(esquerda+direita)/2];
-        
-        while (i <= j) {
-            while (vetorNumeroAleatorio[i] < pivo){
-                i++;
-                while (vetorNumeroAleatorio[j] > pivo){
-                    j--;
-                    if (i <= j){
-                        //swap(i, j);
-                        i++;
-                        j--; 
-                    }
-                }
+        while (esq <= dir) {
+            while (vetorNumeroAleatorio[esq] < pivo){
+                esq++;
+            }
                 
+            while (vetorNumeroAleatorio[dir] > pivo){
+                dir--;
+            }
+            
+            if (esq <= dir){
+                swap = vetorNumeroAleatorio[esq];
+                vetorNumeroAleatorio[esq] = vetorNumeroAleatorio[dir];
+                vetorNumeroAleatorio[dir] = swap;
+                esq++;
+                dir--; 
             }
         }
-        if (esquerda < j){
-            ordenarQuickSort(vetorNumeroAleatorio, esquerda, j);
-        }
+        if (dir > esquerda)
+            ordenarQuickSort(vetorNumeroAleatorio, esquerda, dir);
+        if (esq < direita)
+            ordenarQuickSort(vetorNumeroAleatorio, esquerda, dir);
         
-        if (i < direita){
-            ordenarQuickSort(vetorNumeroAleatorio, i, direita);
-        }
-
         return vetorNumeroAleatorio;
     }
-    /*
-    public static double [] ordenarHeapSort (double[] vetorNumeroAleatorio) {
-        double vetorHeapSort[];
-
-        //Contrução do heap
-        for (int tam = 2; tam <= n; tam++){
-            construir(tam);
-        }
+    
+    public static double ordenarHeapSort (double vetorNumeroAleatorio[], int n) {
+        int i = n / 2, pai, filho;
+        double t;
         
-        //Ordenacao propriamente dita
-        int tam = n;
-        while (tam > 1){
-        swap(1, tam--);
-        reconstruir(tam);
-        }
+        while (true) {
+            if (i > 0 ) {
+                i--;
+                t = vetorNumeroAleatorio[i];
+            } else {
+                n--;
+                if (n <= 0)
+                    t = vetorNumeroAleatorio[n];
+                    vetorNumeroAleatorio[n] = vetorNumeroAleatorio[0];
+            }
 
-        return vetorHeapSort;
-    } */
+            pai = i;
+            filho = (i * 2) + 1;
+
+            while (filho < n) {
+                if ((filho + 1 < n) && (vetorNumeroAleatorio[filho + 1] > vetorNumeroAleatorio[filho])) {
+                    filho++;
+                    if (vetorNumeroAleatorio[filho] > t) {
+                        vetorNumeroAleatorio[pai] = vetorNumeroAleatorio[filho];
+                        pai = filho;
+                        filho = (pai * 2) + 1;
+                    }
+                } else {
+                    break;
+                }
+            }
+            return vetorNumeroAleatorio[pai] = t;
+        }
+    }
 }
